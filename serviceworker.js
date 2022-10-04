4379ee0f887897d80d5acd825cc679d5fa4c86cf
CasperH <83393210+CasperH1234@users.noreply.github.com>
2022-10-04 10:32:27 +0200

    Add files via upload

---
+++ serviceworker.js
@@ -0,0 +1,15 @@
+const cacheName = 'cache-students';
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(['/students/', 'students/index.html', 'students/icon-192.png','students/icon-512','students/morten.png','students/nina.png','students/olivia.png']);
+    })
+  );
+});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.open(cacheName).then(cache => cache.match(event.request))
+    )
+  );
+});
