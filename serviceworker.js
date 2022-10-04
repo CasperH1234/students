adbd590c6d2e28af76ebff02e69183bacf6efca7
CasperH <83393210+CasperH1234@users.noreply.github.com>
2022-10-04 11:12:31 +0200

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -2,7 +2,7 @@ const cacheName = 'cache-students';
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(cacheName).then(function(cache) {
-      return cache.addAll(['/students/', '/students/index.html', '/students/icon-192.png', '/students/javascript.js','/students/members.json','/students/manifest.json','/students/icon-512','/students/morten.png','/students/nina.png','/students/olivia.png']);
+      return cache.addAll(['/students/', '/students/index.html', '/students/icon-192.png', '/students/javascript.js','/students/members.json','/students/manifest.json','/students/icon-512.png','/students/morten.png','/students/nina.png','/students/olivia.png']);
     })
   );
 });
